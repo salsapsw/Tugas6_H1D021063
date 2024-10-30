ccc694e808fa43311935643d5d347bf881edaf21
Salsabillapsw <salsabillaputerisw29@gmail.com>
2024-10-30 20:38:21 +0700

    tugas 6

---
+++ src/app/folder/folder.page.ts
@@ -0,0 +1,23 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-folder',
+  templateUrl: './folder.page.html',
+  styleUrls: ['./folder.page.scss'],
+})
+export class FolderPage implements OnInit {
+  public folder!: string;
+  public showDetails = false;
+  private activatedRoute = inject(ActivatedRoute);
+
+  constructor() {}
+
+  ngOnInit() {
+    this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+
+  toggleDetails() {
+    this.showDetails = !this.showDetails;
+  }
+}
